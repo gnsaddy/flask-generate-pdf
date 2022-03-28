e7af50a3120e2ea89b53500a47d886f6dbcc4953
Aditya Raj <aditya.x510@gmail.com>
2022-03-28 11:10:02 +0530

    update

--- Dockerfile
+++ Dockerfile
@@ -32,7 +32,7 @@ ADD ./entrypoint-local.sh /usr/src/app/entrypoint-local.sh
 RUN chmod +x ./entrypoint-local.sh
 # add app
 EXPOSE 5000
-HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=5 \
-CMD curl -f http://localhost:5000/health || exit 1
+#HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=5 \
+#CMD curl -f http://localhost:5000/health || exit 1
 # run server
 CMD ["sh", "./entrypoint-local.sh"]
--- project/views/generate.py
+++ project/views/generate.py
@@ -1,5 +1,5 @@
 import os
-
+import tempfile
 from flask import Blueprint, jsonify, redirect, url_for, render_template, request, flash, make_response
 from project import print_flush, db, app
 import pdfkit
@@ -9,23 +9,33 @@ generate_bp = Blueprint('generate_bp', __name__, url_prefix='/req-pdf/')
 
 @generate_bp.route('/pdf/', methods=['GET'])
 def ping_pong():
-    # query = db.engine.execute("select id from users")
+    # query = db.engine.execute("select id from tenant_client")
+    # print_flush("flush:", query.fetchone())
+
+    # render template
+    count = 0
+    res = render_template("index.html", count=count)
     options = {
-        'page-size': 'A4',
-        'margin-top': '0.75in',
-        'margin-right': '0.75in',
-        'margin-bottom': '0.75in',
-        'margin-left': '0.75in',
-        'footer-center': 'PDF REPORT FOOTER',
-        'header-center': 'PDF REPORT HEADER'
+        # 'page-size': 'A4',
+        # 'encoding': 'utf-8'
     }
 
-    res = render_template("index.html")
-
-    pdff = pdfkit.from_string(res, False, options=options)
-    response = make_response(pdff)
-    response.headers["Content-Type"] = "application/pdf"
-    # response.headers["Content-Disposition"] = "attachment; filename=output.pdf"
-    response.headers["Content-Disposition"] = "inline; filename=output.pdf"
+    try:
+        pdff = pdfkit.from_string(res, False, options=options)
+    finally:
+        # os.remove(options['header-html'])
+        pass
+    response = build_response(pdff, 'outputs.pdf')
     # return res
+    print_flush("count:", count)
+    return response
+
+
+def build_response(pdf, filename):
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    # response.headers['Content-Disposition'] = (
+    #     'attachment; filename=' + filename)
+    response.headers['Content-Disposition'] = (
+            'inline; filename=' + filename)
     return response
